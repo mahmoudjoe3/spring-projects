fef208bfa2c15eec6b6b0574f61bd5f56959a3ef
mahmoud youssef <49236858+mahmoudjoe3@users.noreply.github.com>
2022-12-28 16:47:59 +0200

    add todo list app init

---
+++ spring-todo-list/src/test/java/com/mahmoudjoe/springtodolist/SpringTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmoudjoe.springtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
